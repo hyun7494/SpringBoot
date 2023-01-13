5f438bc98da0d91fce9080971c880896d4fd753c
et009153 <et009153@gmail.com>
2023-01-13 13:52:26 +0900

    2023/01/13 13:52 학원

--- Ch08/src/main/java/kr/co/ch08/controller/User2Controller.java
+++ Ch08/src/main/java/kr/co/ch08/controller/User2Controller.java
@@ -2,12 +2,20 @@ package kr.co.ch08.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import kr.co.ch08.service.User2Service;
+import kr.co.ch08.vo.User2VO;
 
 @Controller
 public class User2Controller {
 
+	@Autowired
+	private User2Service service;
+	
 	@GetMapping("/user2/login")
 	public String login() {
 		return"/user2/login";
@@ -21,6 +29,24 @@ public class User2Controller {
 	@GetMapping("/user2/logout")
 	public String logout(HttpSession sess) {
 		sess.invalidate();		
+		return "redirect:/user2/login";
+	}
+	/*
+	@GetMapping("/user2/register")
+	public String register() {
+		return "/user2/register";
+	}
+	*/
+	
+	@GetMapping("/user2/register")
+	public void register() {}
+	
+	
+	@PostMapping("/user2/register")
+	public String register(User2VO vo) {
+		
+		service.insertUser2(vo);
+		
 		return "redirect:/user2/login";
 	}
 }
---
+++ Ch08/src/main/java/kr/co/ch08/security/MyUserDetails.java
@@ -0,0 +1,73 @@
+package kr.co.ch08.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MyUserDetails implements UserDetails{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String uid;
+	private String pass;
+	private String name;
+	private String hp;
+	private int age;
+	private String rdate;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// 계정이 갖는 권한 목록 리턴
+		Collection<GrantedAuthority> collector = new ArrayList<>();
+		return collector;
+	}
+
+	@Override
+	public String getPassword() {
+		// 계정이 갖는 비밀번호
+		return pass;
+	}
+
+	@Override
+	public String getUsername() {
+		// 계정이 갖는 아이디(uid)
+		return uid;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// 계정 만료 여부 : true -> 만료 안됨, false -> 만료 (NonExpired)
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// 계정 잠김 여부(true: 잠김 X, false: 잠김)
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// 계정 비밀번호 만료 여부 (true: 만료 X, false: 만료)
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// 계정 활성화 여부 (true: 활성화, false: 비활성화)
+		return true;
+	}
+
+	
+
+}
